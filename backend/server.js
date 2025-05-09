c3c363dc3db1e1b6b8c7996da709744daad0e238
Meghana <meghanamaggie0987@gmail.com>
2025-05-09 12:07:46 +0530

    changes applied

--- backend/server.js
+++ backend/server.js
@@ -70,13 +70,15 @@ app.get('/routes', async (req, res) => {
     res.status(500).json({ error: 'Internal server error' });
   }
 });
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "frontend", "build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  });
-}
+
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "frontend", "build")));
+
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+//   });
+// }
 
 
 app.listen(port, () => {
