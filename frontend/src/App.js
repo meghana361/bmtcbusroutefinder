55f5e0cf60e378e3adc9a905b9d14e04a3327dba
Meghana <meghanamaggie0987@gmail.com>
2025-05-10 20:29:57 +0530

    fix: regenerate prisma client

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import RouteFinder from './components/RouteFinder';
 function App() {
   return (
     <div className="App" >
-      <h1 style={{marginLeft:"670px"}}>Bus Route Finder</h1>
+
       <RouteFinder  />
     </div>
   );
